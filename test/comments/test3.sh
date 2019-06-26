ce5625f50a2dc8e2e25283640fb9463efb41e2cc
ChillerDragon <ChillerDragon@gmail.com>
2019-06-26 15:43:55 +0200

    Improve tests for comments

---
+++ test/comments/test3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# I am a single line comment
+echo "h#sh in comment"
--- test/test.sh
+++ test/test.sh
@@ -7,23 +7,43 @@ Green='\033[0;32m'
 
 run=../../shelliflyer.sh
 
+errors=0
+passed=0
+
 function assert_equals() {
   d=$(diff <($1) <($2))
   if [ "$d" ]
   then
     echo -e "[${Red}-${Reset}] error:"
     echo "diff: $d"
+    errors=$((errors + 1))
   else
     echo -e "[${Green}+${Reset}]"
+    passed=$((passed + 1))
   fi
 }
 
 cd comments
-# test 1
+
 $run test1.sh out1.sh
 chmod +x out1.sh
 assert_equals ./test1.sh ./out1.sh
-# test 2
+
 $run test2.sh out2.sh
 chmod +x out2.sh
 assert_equals ./test2.sh ./out2.sh
+
+$run test3.sh out3.sh
+chmod +x out3.sh
+assert_equals ./test3.sh ./out3.sh
+
+echo "---------------------"
+echo "tests failed:  $errors"
+echo "tests passsed: $passed"
+
+if [ $errors -gt 0 ]
+then
+    echo -e "[${Red}ERROR${Reset}] test run failed."
+else
+    echo -e "[${Green}SUCCESS${Reset}] all tests passed."
+fi
